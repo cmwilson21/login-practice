e57e193b586395af4e1308db8c1ce86c94ddb108
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-14 21:55:23 -0600

    added handleChange

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = () => {
+  const [form, setForm] = useState({
+    username:"",
+    password:""
+  })
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
 
 
 
   return (
     <div>
       <form>
-        <input type="text" name="username" placeholder="username" />
-        <input type="password" name="password" placeholder="password" />
+        <input type="text" name="username" placeholder="username" onChange={handleChange}/>
+        <input type="password" name="password" placeholder="password" onChange={handleChange} />
         <button type="submit" placeholder="Login">Login</button>
       </form>
       
